17345df6295844db1a1f93284483b637f6813b8d
Evan Leach <evan.leach@disney.com>
2017-12-13 11:24:43 -0800

    initial commit

---
+++ WhatShouldIPlay/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WhatShouldIPlay.Startup))]
+namespace WhatShouldIPlay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
